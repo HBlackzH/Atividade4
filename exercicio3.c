432e3f16e768556dafcf94ed79fd6a53ba8ce238
Caio Henrique <142325254+HBlackzH@users.noreply.github.com>
2023-09-01 19:08:03 -0300

    exercicio3.c

--- exercicio3.c
+++ exercicio3.c
@@ -2,31 +2,30 @@
 #include<math.h>
 
 int main(){
-    int A= 10, B=20, C=30;
-
-   
+    
+int A= 10, B=20, C=30;
 
     if (A <= B && A <= C) {
         printf("%d ", A);
-        if (B <= C) {
-            printf("%d %d\n", B, C);
-        } else {
-            printf("%d %d\n", C, B);
-        }
+    if (B <= C) {
+        printf("%d %d\n", B, C);
+    } else {
+        printf("%d %d\n", C, B);
+    }
     } else if (B <= A && B <= C) {
         printf("%d ", B);
-        if (A <= C) {
-            printf("%d %d\n", A, C);
-        } else {
-            printf("%d %d\n", C, A);
-        }
+    if (A <= C) {
+        printf("%d %d\n", A, C);
+    } else {
+         printf("%d %d\n", C, A);
+    }
     } else if (C <= A && C <= B) {
         printf("%d ", C);
-        if (A <= B) {
-            printf("%d %d\n", A, B);
-        } else {
-            printf("%d %d\n", B, A);
-        }
+    if (A <= B) {
+        printf("%d %d\n", A, B);
+    } else {
+        printf("%d %d\n", B, A);
+    }
     }
 
 }
